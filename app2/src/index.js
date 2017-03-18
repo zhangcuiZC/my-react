a3297275bc111978a4d906f71e1c524e5a0d2444
ZhangCui <zczhangcui@foxmail.com>
2017-03-18 15:57:51 +0800

    update

--- app2/src/index.js
+++ app2/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 // import App from './App';
 // import './index.css';
@@ -62,8 +62,100 @@ import ReactDOM from 'react-dom';
 // 	document.getElementById('root')
 // );
 
-import MyComponent from './MyComponent.js';
+// import MyComponent from './MyComponent.js';
+// ReactDOM.render(
+// 	<MyComponent />,
+// 	document.getElementById('root')
+// );
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, bindActionCreators } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+//action
+function changeText(){
+	return { type : 'CHANGE_TEXT' };
+}
+
+function buttonClick(){
+	return { type : 'BUTTON_CLICK' };
+}
+
+//reducer
+const initialState = { text : 'Hello' };
+
+function myApp(state = initialState, action){
+	switch(action.type) {
+		case 'CHANGE_TEXT':
+			return { text : state.text === 'Hello' ? 'Stark' : 'Hello' };
+		case 'BUTTON_CLICK':
+			return { text : state.text === 'you just click the button.' ? 'Hello' : 'you just click the button.' };
+		default :
+			return { text : 'Hello' };
+	}
+}
+
+//store
+let store = createStore(myApp);
+
+//component
+class Hello extends Component {
+	handleClick = () => {
+		this.props.actions.changeText();
+	}
+
+	render() {
+		return (
+			<h1 onClick={this.handleClick}>{this.props.text}</h1>
+		);
+	}
+}
+
+class Change extends Component {
+	handleClick = () => {
+		this.props.actions.buttonClick();
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>Change</button>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Hello actions={this.props.actions} text={this.props.text} />
+				<Change actions={this.props.actions} />
+			</div>
+		);
+	}
+}
+
+//map
+function mapStateToProps(state){
+	return { text : state.text };
+}
+function mapDispatchToProps(dispatch){
+	return {
+		actions : bindActionCreators({ changeText : changeText, buttonClick : buttonClick }, dispatch)
+	};
+}
+
+App = connect(mapStateToProps, mapDispatchToProps)(App);
+
+//render
 ReactDOM.render(
-	<MyComponent />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
+
+
+
+
+
