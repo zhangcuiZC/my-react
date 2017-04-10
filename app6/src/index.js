52dbee39c8ed1efd187029126a610e0a26223923
ZC <zczhangcui@foxmail.com>
2017-04-10 14:55:54 +0800

    update

--- app6/src/index.js
+++ app6/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { getAllProducts } from './actions';
+import App from './containers/App';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+store.dispatch(getAllProducts());
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  	<App/>
+  </Provider>,
   document.getElementById('root')
 );
